8d3cfb114ace56ea441de4127fac101dc69e2a22
bublik <bublik.script@gmail.com>
2023-11-26 22:34:56 +0200

    minor

--- src/App.tsx
+++ src/App.tsx
@@ -36,7 +36,7 @@ export default function App() {
 
         while (true) {
           const player = prompt(
-            `Food: ${food}\n\nEnter your name: `,
+            `🍗Food: ${food}\n\nEnter your name: `,
             defaultName.current ?? undefined
           );
 
